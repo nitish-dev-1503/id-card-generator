3cc6a777a54c9d6dda989c3e1c2d7e13da52b93f
Nitish Sharma <smartnitish15@gmail.com>
2022-09-07 13:26:25 +0530

    Add service to generate pdf from template file

---
+++ service/card_generator.py
@@ -0,0 +1,19 @@
+import pdfkit
+
+
+def pdfGenerator(file, id, name, role, picture):
+    fileAsString = file.read()
+    fileAsString = fileAsString.replace('@ID', id)
+    fileAsString = fileAsString.replace('@NAME', name)
+    fileAsString = fileAsString.replace('@ROLE', role)
+    fileAsString = fileAsString.replace('@PICTURE', picture)
+
+    return convertToPdf(fileAsString)
+
+
+def convertToPdf(template):
+    config = pdfkit.configuration(wkhtmltopdf='/usr/local/bin/wkhtmltopdf')
+    options = {'page-height': '525px', 'page-width': '360px', 'margin-left': '0mm',
+               'margin-right': '0mm', 'margin-top': '0mm', 'margin-bottom': '0mm'}
+
+    return pdfkit.from_string(template, configuration=config, options=options)

05094bbc38bb66fb8a4f9f5725400d3c3538cd41
Nitish Sharma <smartnitish15@gmail.com>
2022-09-07 13:23:27 +0530

    Add home page UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./templates/**/*.html",
+    "./static/src/**/*.js"
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#3A3A3A',
+        secondary: '#F5F5F5',
+        accent: '#E5E5E5'
+      },
+      fontFamily: {
+        'ubuntu': ['Ubuntu', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
